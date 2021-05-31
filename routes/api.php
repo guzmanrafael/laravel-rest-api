5476865887ca5ea56fb0a58b89a521bbea7cc4b3
guzmanrafael <rafael.guzman1609@gmail.com>
2021-05-31 08:24:15 -0700

    initial project

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/reset', 'ResetController@reset');
+Route::get('/balance', 'BalanceController@show');
+Route::post('/event', 'EventController@store');
